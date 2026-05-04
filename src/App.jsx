66d708cd43158e9245256fcce92f91b28cad5124
hamsfd143-tech <hamsfd143@gmail.com>
2026-05-04 06:37:30 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>MakhzanLink Oman</h1>
+      <p>منصة التخزين والنقل التشاركي</p>
+    </div>
+  );
+}
